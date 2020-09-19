5d3ee6c86f74efc5aa1826785dca2415870a29d5
ms-kang <imyekaterin3@naver.com>
2020-09-19 15:19:20 +0900

    first commit

---
+++ bluetooth_connect/bluetooth_connect.ino
@@ -0,0 +1,112 @@
+#include <SoftwareSerial.h>
+#include <SPI.h>
+#include <MFRC522.h>
+
+//블루투스모듈 HC-06(슬래이브만가능)으로 진행함 
+//블루투스모듈 HC-05(슬래이브 마스터둘다가능)는 조금 코드가 다르다  
+//HC-06 시리얼창에서 "line ending 없음" 설정할것
+
+#define SS_PIN 10
+#define RST_PIN 9
+
+MFRC522 rfid(SS_PIN, RST_PIN); // Instance of the class
+
+MFRC522::MIFARE_Key key; 
+
+byte nuidPICC[4];
+
+int Tx = 7; //전송 보내는핀  
+int Rx = 6; //수신 받는핀
+
+SoftwareSerial BtSerial(Tx,Rx);
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println("Ready to go");
+  SPI.begin(); // Init SPI bus
+
+  for (byte i = 0; i < 6; i++) {
+    key.keyByte[i] = 0xFF;
+  }
+
+  Serial.println(F("This code scan the MIFARE Classsic NUID."));
+  Serial.print(F("Using the following key:"));
+  printHex(key.keyByte, MFRC522::MF_KEY_SIZE);
+  
+  rfid.PCD_Init(); // Init MFRC522 
+  BtSerial.begin(9600);
+  }
+
+void loop() {
+   // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
+   
+  if ( ! rfid.PICC_IsNewCardPresent())
+    return;
+
+  // Verify if the NUID has been readed
+  if ( ! rfid.PICC_ReadCardSerial())
+    return;
+
+  Serial.print(F("PICC type: "));
+  MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
+  Serial.println(rfid.PICC_GetTypeName(piccType));
+
+  // Check is the PICC of Classic MIFARE type
+  if (piccType != MFRC522::PICC_TYPE_MIFARE_MINI &&  
+    piccType != MFRC522::PICC_TYPE_MIFARE_1K &&
+    piccType != MFRC522::PICC_TYPE_MIFARE_4K) {
+    Serial.println(F("Your tag is not of type MIFARE Classic."));
+    return;
+  }
+
+  if (rfid.uid.uidByte[0] != nuidPICC[0] || 
+    rfid.uid.uidByte[1] != nuidPICC[1] || 
+    rfid.uid.uidByte[2] != nuidPICC[2] || 
+    rfid.uid.uidByte[3] != nuidPICC[3] ) {
+    easy_print("A new card has been detected.");
+    Serial.println(F("A new card has been detected."));
+    BtSerial.println(F("A new card has been detected."));
+
+    // Store NUID into nuidPICC array
+    for (byte i = 0; i < 4; i++) {
+      nuidPICC[i] = rfid.uid.uidByte[i];
+    }
+   
+    Serial.println(F("The NUID tag is:"));
+    Serial.print(F("In hex: "));
+    printHex(rfid.uid.uidByte, rfid.uid.size);
+    Serial.println();
+    Serial.print(F("In dec: "));
+    printDec(rfid.uid.uidByte, rfid.uid.size);
+    Serial.println();
+  }
+  else Serial.println(F("Card read previously."));
+
+  // Halt PICC
+  rfid.PICC_HaltA();
+
+  // Stop encryption on PCD
+  rfid.PCD_StopCrypto1();
+
+}
+
+void printHex(byte *buffer, byte bufferSize) {
+  for (byte i = 0; i < bufferSize; i++) {
+    Serial.println(buffer[i] < 0x10 ? " 0" : " ");
+    Serial.println(buffer[i], HEX);
+  }
+}
+int easy_print(String text) {
+    Serial.println(text);
+    BtSerial.println(text);
+}
+/**
+ * Helper routine to dump a byte array as dec values to Serial.
+ */
+void printDec(byte *buffer, byte bufferSize) {
+  for (byte i = 0; i < bufferSize; i++) {
+    Serial.print(buffer[i] < 0x10 ? " 0" : " ");
+    Serial.print(buffer[i], DEC);
+  }
+}
